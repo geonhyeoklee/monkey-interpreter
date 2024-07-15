b3f9d077d4466cd55e52e4e7ce945125122ab111
GeonHyeokLee <geonhyeoklee.kr@gmail.com>
2024-07-15 12:56:58 +0900

    feat: add compile detail

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -6,6 +6,11 @@ import (
 	"monkey/object"
 )
 
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants    []object.Object
+}
+
 type Compiler struct {
 	instructions code.Instructions
 	constants    []object.Object
@@ -19,6 +24,37 @@ func New() *Compiler {
 }
 
 func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, s := range node.Statements {
+			err := c.Compile(s)
+			if err != nil {
+				return err
+			}
+		}
+
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
+
+	case *ast.InfixExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+
+		err = c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
+
+	case *ast.IntegerLiteral:
+		integer := &object.Integer{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(integer))
+	}
+
 	return nil
 }
 
@@ -29,7 +65,19 @@ func (c *Compiler) Bytecode() *Bytecode {
 	}
 }
 
-type Bytecode struct {
-	Instructions code.Instructions
-	Constants    []object.Object
+func (c *Compiler) addConstant(obj object.Object) int {
+	c.constants = append(c.constants, obj)
+	return len(c.constants) - 1
+}
+
+func (c *Compiler) emit(op code.Opcode, operands ...int) int {
+	ins := code.Make(op, operands...)
+	pos := c.addInstruction(ins)
+	return pos // 새로 만든 명령어의 시작 위치를 반환
+}
+
+func (c *Compiler) addInstruction(ins []byte) int {
+	posNewInstruction := len(c.instructions)
+	c.instructions = append(c.instructions, ins...)
+	return posNewInstruction
 }
