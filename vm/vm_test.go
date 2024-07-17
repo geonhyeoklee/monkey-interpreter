ff46c9dea42393b4d011005d19f311aa98a1a7ab
GeonHyeokLee <geonhyeoklee.kr@gmail.com>
2024-07-17 19:30:34 +0900

    feat: add 'OpMinus' and 'OpBang' code

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -15,22 +15,6 @@ type vmTestCase struct {
 	expected interface{}
 }
 
-func TestBooleanExpressions(t *testing.T) {
-	tests := []vmTestCase{
-		{"true", true},
-		{"false", false},
-		{"1 < 2", true},
-		{"1 > 2", false},
-		{"1 == 2", false},
-		{"1 == 1", true},
-		{"true == true", true},
-		{"true == false", false},
-		{"true != false", true},
-	}
-
-	runVmTests(t, tests)
-}
-
 func TestIntegerArithmetic(t *testing.T) {
 	tests := []vmTestCase{
 		{"1", 1},
@@ -45,6 +29,31 @@ func TestIntegerArithmetic(t *testing.T) {
 		{"5 * 2 + 10", 20},
 		{"5 + 2 * 10", 25},
 		{"5 * (2 + 10)", 60},
+		{"-5", -5},
+		{"-50 + 100 + -50", 0},
+		{"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
+	}
+
+	runVmTests(t, tests)
+}
+
+func TestBooleanExpressions(t *testing.T) {
+	tests := []vmTestCase{
+		{"true", true},
+		{"false", false},
+		{"1 < 2", true},
+		{"1 > 2", false},
+		{"1 == 2", false},
+		{"1 == 1", true},
+		{"true == true", true},
+		{"true == false", false},
+		{"true != false", true},
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
 	}
 
 	runVmTests(t, tests)
